740f7e190d477aac640df38ad6d5641895e97632
Raphael Trajano <raphaelsrt@gmail.com>
2022-11-03 12:10:07 -0300

    refactor: test and code popover in SummaryForm

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,9 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
 describe("Checkbox tests", () => {
@@ -14,7 +19,9 @@ describe("Checkbox tests", () => {
     expect(purchaseButton).toBeDisabled();
   });
 
-  test("Checkbox enable and disable button", () => {
+  test("Checkbox enable and disable button", async () => {
+    const user = userEvent.setup();
+
     render(<SummaryForm />);
     const checkbox = screen.getByRole("checkbox", {
       name: /terms and conditions/i,
@@ -22,10 +29,34 @@ describe("Checkbox tests", () => {
     const purchaseButton = screen.getByRole("button", {
       name: "Confirm order",
     });
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(checkbox).toBeChecked();
 
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(purchaseButton).toBeDisabled();
+  });
+
+  test("popover responds to hover", async () => {
+    const user = userEvent.setup();
+    render(<SummaryForm />);
+
+    // popover starts out hidden
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopover).not.toBeInTheDocument();
+
+    // popover appears on mouseover of checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    await user.hover(termsAndConditions);
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
+
+    // popover disappears when we mouse out
+    await user.unhover(termsAndConditions);
+    await waitForElementToBeRemoved(popover);
+    expect(popover).not.toBeInTheDocument();
   });
 });
